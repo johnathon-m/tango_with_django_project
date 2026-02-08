fc28ce49678bcd957291253d17a015c82ffe7660
“Johnathon <3013701m@student.gla.ac.uk>
2026-02-08 19:53:51 +0000

    Chapter 9: Before exercises/added logout functionality

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,8 @@ from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -210,3 +211,16 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
         return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+# Use the login_required() decorator to ensure only those logged in can
+# access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the hompage.
+    return redirect(reverse('rango:index'))
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -128,3 +128,5 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 # Media 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+LOGIN_URL = 'rango:login'
