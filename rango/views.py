7556fcd425ce0cd05cf28993d4d8b82afb3384b3
“Johnathon <3013701m@student.gla.ac.uk>
2026-02-12 21:39:38 +0000

    Chapter 10 Complete: All tests passed

--- rango/views.py
+++ rango/views.py
@@ -24,13 +24,15 @@ def index(request):
 
     # Call the helper function to handle the cookies
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
 
     response = render(request, 'rango/index.html', context=context_dict)
     return response
 
 def about(request):
-    return render(request, 'rango/about.html') 
+    visitor_cookie_handler(request)
+    context_dict = {}
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'rango/about.html', context=context_dict) 
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
